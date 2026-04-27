154ef3402b766a2afdd63f35325d826045a80b83
anushreediwakarece <anushreediwakarece@gmail.com>
2026-04-27 22:31:56 +0530

    Add files via upload

---
+++ day6.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std ; 
+void  Counter ()
+ {
+    static int count ; //without static keyword , the function will print only 0 
+    //now after static it will print 1,2,3 . 
+    count ++; 
+    cout<<" count = "<<count <<endl ; 
+
+ }
+ int main ()
+ {
+    Counter(); 
+    Counter (); 
+     Counter (); 
+     return 0 ; 
+ }
