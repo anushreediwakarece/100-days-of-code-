88f18b3c9a980ceabc081a9240d17b79e191e06d
anushreediwakarece <anushreediwakarece@gmail.com>
2026-05-23 20:49:27 +0530

    Add files via upload

---
+++ day29.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ; 
+int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    cout<<fact ; 
+    return fact;
+}
+int main()
+{ factorial(5); 
+    int fact = 1 ; 
+}
