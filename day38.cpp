9939ce1d6018b3aa93a7688fe6eb8c7f29281c8e
anushreediwakarece <anushreediwakarece@gmail.com>
2026-06-11 12:23:15 +0530

    Add files via upload

---
+++ day38.cpp
@@ -0,0 +1,27 @@
+//kadane algorithm 
+#include<iostream>
+#include<climits>
+using namespace std ; 
+void maxsubarraysum (int arr[] , int n )
+{
+    int maxsum = INT_MIN ; 
+    int currsum = 0 ; 
+    for(int i =0 ; i<n ; i++)
+    {
+        currsum += arr[i] ; 
+        maxsum = max( maxsum , currsum);
+        if(currsum<0)
+        {
+            currsum = 0 ; 
+        }
+
+    }
+    cout<<"max subarray sum is "<<maxsum <<endl ; 
+    
+}
+int main()
+{
+    int arr[]= { 2,-3, 6, -5,4,2}; 
+    int n = sizeof(arr)/ sizeof(int); 
+maxsubarraysum (arr, n ); 
+}
