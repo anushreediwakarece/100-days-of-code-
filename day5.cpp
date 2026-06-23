ab499356be2ba6ce5e901356d9199b6c9004a457
anushreediwakarece <anushreediwakarece@gmail.com>
2026-06-23 16:17:02 +0530

    Add files via upload

--- day5.cpp
+++ day5.cpp
@@ -1,35 +1,17 @@
 #include<iostream>
 using namespace std ; 
-class Counter{
-    public: 
-    int value  ; 
-    // overload function , will return bool because (a==b )is either true or false 
-    bool operator ==(Counter other )
-    {
-  Counter result ; //creating a variable to store the value true or false 
-  if (this->value == other.value )
-  { 
-    return true; 
-  }
-  else 
-  {
- return false ; 
-  }
-
-    }
-}; 
 int main()
 {
- Counter c1, c2 ; 
- c1.value = 5 ; 
- c2. value = 3 ;
- //calling the overloaded function 
- if(c1==c2)
- {
-    cout<< "the values are true "<<endl; 
- }
- else{
-    cout<<" the values are not equal "<<endl ; 
- }
-return 0; 
+int n ; 
+cout<<"enter n "<<endl ; 
+cin>>n ; 
+for( int i =n ; i<(n+1) ; i--)
+{
+    for(int j =1 ; i<=i ; j++)
+    {
+        cout<<"*"<<" "; 
+    }
+    cout<<endl ; 
+}
+return 0 ; 
 }
