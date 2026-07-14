3c4eb2ed2cd6d8530032c611ffd717651abb515c
anushreediwakarece <anushreediwakarece@gmail.com>
2026-07-14 22:51:42 +0530

    Add files via upload

--- day59.cpp
+++ day59.cpp
@@ -1,42 +1,37 @@
-//binary search code : 
+//bubblesort code : 
 #include<iostream>
 using namespace std ; 
-int bi( int arr[] , int n , int key )
-{
-      int st = 0;
-      int end = n - 1;
-      while( st <= end)
+void print (int arr[] , int n ) 
+{ 
+    // function to print the sorted array 
+    for(int i =0 ;i<n ; i++)
     {
-        int mid = ( st+end)/2; 
-        if(arr[mid] ==key )
-        {
-            return mid ; 
-        }
-        else if (arr[mid]< key )
-        {
-            st = mid+1 ; 
-        }
-        else 
-        {
-             end= mid-1 ; 
-        }
+        cout<<arr[i]<<" ";
     }
-return -1 ;  
 }
-    int main()
-    { int arr[] = { 2,3,4,5,6,7,8,9}; 
-    int n = sizeof(arr)/ sizeof(int ); 
-    int key ; 
-    cout<<"enter key "; 
-    cin>>key ; 
-
-    
-        int st , end ; 
-        int st = 0 ; 
-        int end = n-1 ; 
-        int mid ; 
-        cout<<bi(arr, n , key ); 
-        cout<<endl ; 
-        return 0; 
-
+void bubble( int arr[] , int n )
+{
+    for(int i =0 ; i<n-1 ; i++)
+    {
+        for(int j = 0 ;j<n-i-1 ; j++)
+        {
+            if(arr[j]< arr[j+1])
+            {
+                swap(arr[j] , arr[j+1]); 
+            }
+        }
     }
+
+    print( arr , n );
+
+}
+
+int main()
+{
+    int arr[] ={ 5,4,3, 1,2 }; 
+    int n = sizeof(arr)/sizeof(int);
+    bubble(arr, n); 
+    return 0 ; 
+
+
+}
