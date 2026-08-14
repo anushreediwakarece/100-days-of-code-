95997aad48f3b1c25180f90f5dab8733accde101
Anushree Diwakar <anushreediwakarece@gmail.com>
2026-08-15 00:13:59 +0530

    pattern code

---
+++ day83.cpp
@@ -0,0 +1,18 @@
+// pattern printing code :
+#include<iostream>
+using namespace std ; 
+int main()
+{
+    int n ; 
+    cout<<"enter the number of rows :";
+    cin>>n;
+    for(int i =0 ; i<n ; i++)
+    {
+        for(int j =0 ; j<=i ; i++)
+        {
+            cout<<"*"; 
+        }
+        cout<<endl;
+    }
+    return 0 ; 
+}
